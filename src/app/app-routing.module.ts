2683a868cc85263a83e80bd8200acc48a437f021
SilvioDiaz <57494579+SilvioDiaz@users.noreply.github.com>
2021-07-22 22:52:23 -0300

    Bas de Routing funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharacterDetailsComponent } from './character-details/character-details.component';
 import { CharactersComponent } from './characters/characters.component';
 
 const routes: Routes = [
-  {path: '',component:CharactersComponent}
+  {path: '',component:CharactersComponent},
+  {path: 'character/:id', component:CharacterDetailsComponent}
 ];
 
 @NgModule({
