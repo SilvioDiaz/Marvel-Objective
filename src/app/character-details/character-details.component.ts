895237e77a33888f6644c5fb17e0dcee06ad1fe8
SilvioDiaz <57494579+SilvioDiaz@users.noreply.github.com>
2021-07-22 22:54:29 -0300

    Resgatando detalhes de personagem

--- src/app/character-details/character-details.component.ts
+++ src/app/character-details/character-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ServerService } from '../server.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-character-details',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharacterDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private detailsSvc:ServerService, private route:ActivatedRoute ) { }
+
+  characterDetails: Observable<any>
+  id:string = this.route.snapshot.params.id
 
   ngOnInit(): void {
+    this.getDetails()
   }
 
+  getDetails(){
+    this.characterDetails = this.detailsSvc.getCharacterDetails(this.id)
+  }
 }
