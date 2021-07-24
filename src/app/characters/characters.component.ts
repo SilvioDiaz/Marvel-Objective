36fe77638046a7a844968abe8b4cd4d749dba6d9
SilvioDiaz <57494579+SilvioDiaz@users.noreply.github.com>
2021-07-24 00:28:08 -0300

    Melhoria de código de requisição de personagem

--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -13,40 +13,49 @@ export class CharactersComponent implements OnInit {
   constructor(private charactersSvc:ServerService) { }
 
   allCharacters: Observable<any>
+  characters:Observable<any>
   pageNumbers:number
   currentPage: number = 1
   heroName: string
 
   ngOnInit(): void {
     this.getCharacters()
-    this.getPagesNumber()
   }
+
+  getObservable(){ //Chama  número de paginas e guarda dados de personagens
+    this.getPagesNumber()
+    this.characters = this.allCharacters.pipe(map((data:any) => data.results))
+  }
+
+  getPagesNumber(){ // Recebe total de paginas de AllCharacters
+    this.allCharacters.subscribe(x => {
+      this.pageNumbers = x.total
+    })
+  }
+
+  //REQUISIÇÕES
 
   searchHero(text:string){//Requisição com pesquisa de nome do herói
     this.heroName = text
 
     this.allCharacters = this.charactersSvc.getAllCharacters(this.currentPage,text)
-    .pipe(map((data:any) => data.data.results))
+    .pipe(map((data:any) => data.data))
 
-    this.getPagesNumber()
+    this.getObservable()
   }
 
   pageChange(number: number) {//Requisição novos personagens ao trocar de pagina
     this.allCharacters = this.charactersSvc.getAllCharacters(number,this.heroName)
-    .pipe(map((data:any) => data.data.results))
+    .pipe(map((data:any) => data.data))
+
+    this.getObservable()
   }
 
   getCharacters(){ // Envia para allCharacters os valores da requisição com todos os personagens
     this.allCharacters = this.charactersSvc.getAllCharacters(this.currentPage,this.heroName)
-    .pipe(map((data:any) => data.data.results))
-  }
+    .pipe(map((data:any) => data.data))
 
-  getPagesNumber(){ //Requisição número total de paginas para o pagination
-    this.charactersSvc.getAllCharacters(this.currentPage,this.heroName).subscribe(
-      (data) => {
-        this.pageNumbers = data.data.total
-      }
-    )
+    this.getObservable()
   }
 
 }
